997519b3d0cd4df41b436799576fd8015c888fd6
asadsanto10 <santo.meridian@gmail.com>
2020-07-22 00:40:56 +0600

    add style

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ const loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function () { 
     const loginArea = document.getElementById("login_area");
     loginArea.style.display = "none";
+    const tarnsactionArea = document.getElementById('transaction_area');
+    tarnsactionArea.style.display = 'block';
 });
